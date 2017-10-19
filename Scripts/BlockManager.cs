e5f6d0c79768ef45f2d5b26e143dce5d2536ce93
KirbyXcode <kirby_yu@hotmail.com>
2017-10-19 14:20:19 +0800

    Add files via upload

---
+++ Scripts/BlockManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uniblocks;
+
+public class BlockManager : MonoBehaviour 
+{
+    public float range = 5;
+    private ushort blockID = 0;
+    private Transform selectEffect;
+    private MeshRenderer selectRender;
+
+    void Start()
+    {
+        selectEffect = GameObject.Find("selected block graphics").transform;
+        selectRender = selectEffect.GetComponent<MeshRenderer>();
+        selectRender.enabled = false;
+    }
+
+    private void SelectBlock()
+    {
+        for (ushort i = 0; i < 9; i++)
+        {
+            if(Input.GetKeyDown(i.ToString()))
+            {
+                blockID = i;
+            }
+        }
+    }
+
+    private void UpdateSelectEffect(VoxelInfo info)
+    {
+        if(info !=null)
+        {
+            selectEffect.position = info.chunk.VoxelIndexToPosition(info.index);
+            selectRender.enabled = true;
+        }
+        else
+        {
+            selectRender.enabled = false;
+        }
+    }
+
+	void Update () 
+	{
+        SelectBlock();
+
+        Transform camTrans = Camera.main.transform;
+
+        VoxelInfo info = Engine.VoxelRaycast(camTrans.position, camTrans.forward, range, false);
+        
+        if(info != null)
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                Voxel.DestroyBlock(info);
+            }
+
+            if(Input.GetMouseButtonDown(1))
+            {
+                VoxelInfo neighbourInfo = new VoxelInfo(info.adjacentIndex, info.chunk);
+                Voxel.PlaceBlock(neighbourInfo, blockID);
+            }
+        }
+
+        UpdateSelectEffect(info);
+	}
+}
---
+++ Scripts/MapManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Uniblocks;
+
+public class MapManager : MonoBehaviour 
+{
+    private Transform playerTrans;
+    private Index lastIndex;
+
+	void Start () 
+	{
+        playerTrans = GameObject.FindWithTag("Player").transform;
+
+        InvokeRepeating("GenerateMap", 1, 0.5f);
+	}
+
+    private void GenerateMap()
+    {
+        if (Engine.Initialized == false || ChunkManager.Initialized == false) return;
+
+        Index currentIndex = Engine.PositionToChunkIndex(playerTrans.position);
+
+        if (lastIndex != currentIndex)
+        {
+            ChunkManager.SpawnChunks(playerTrans.position);
+            lastIndex = currentIndex;
+        }
+    }
+
+    private void SaveMap()
+    {
+        Engine.SaveWorld();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+            SaveMap();
+    }
+}
